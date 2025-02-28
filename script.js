0f70360ea5880fc22a319199f7a29f39d5c4203c
Affan <affandeveloper3424@gmail.com>
2025-02-28 20:50:19 +0530

    set up constants and event listeners

---
+++ script.js
@@ -0,0 +1,36 @@
+// API Key
+
+// http://api.weatherapi.com/v1/current.json?key=5312652ec7f74e7d87440402252102&q=Midnapur&aqi=no
+
+
+// ELEMENTS
+
+// const elements = {
+//     input: document.querySelector(".search-container input"),
+//     searchBtn: document.querySelector(".search-btn") ,
+//     locationText: document.querySelector("#result h2"),
+//     weather: document.querySelector("#result .weather"),
+//     weatherDesc: document.querySelector("#result .desc"),
+//     weatherImg: document.querySelector("#result img"),
+//     weatherDeg: document.querySelector("#result h1"),
+//     minTemp: document.querySelector(".min_temp"),
+//     maxTemp: document.querySelector(".max_temp"), 
+// }
+
+// // CONSTANTS
+
+// const baseUrl = 'http://api.weatherapi.com/v1/current.json?key=5312652ec7f74e7d87440402252102'
+
+// async function fetchWeather(location){
+//     const  stringResponse = await fetch(`${baseUrl}&q=${Midnapur}&aqi=no`)
+//     const response = await stringResponse.json()
+    
+// } 
+
+
+// // EVENT LISTENERS
+
+// elements.searchBtn.addEventListener("click", ()=>{
+//     fetchWeather(elements.input.value)
+// })
+
