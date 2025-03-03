8fef2013dc75c7fe39f39349a4b8f157cc4d8ed5
Affan <affandeveloper3424@gmail.com>
2025-03-03 10:35:15 +0530

    show weather to the user

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // API Key
 
 // http://api.weatherapi.com/v1/current.json?key=5312652ec7f74e7d87440402252102&q=Midnapur&aqi=no
@@ -5,32 +6,53 @@
 
 // ELEMENTS
 
-// const elements = {
-//     input: document.querySelector(".search-container input"),
-//     searchBtn: document.querySelector(".search-btn") ,
-//     locationText: document.querySelector("#result h2"),
-//     weather: document.querySelector("#result .weather"),
-//     weatherDesc: document.querySelector("#result .desc"),
-//     weatherImg: document.querySelector("#result img"),
-//     weatherDeg: document.querySelector("#result h1"),
-//     minTemp: document.querySelector(".min_temp"),
-//     maxTemp: document.querySelector(".max_temp"), 
-// }
+const elements = {
+    input: document.querySelector(".search-container input"),
+    searchBtn: document.querySelector("#search-btn") ,
+    locationText: document.querySelector("#result h2"),
+    weather: document.querySelector("#result .weather"),
+    humidity: document.querySelector("#result .humidity"),
+    img: document.querySelector("#result img"),
+    weatherImg: document.querySelector("#result img"),
+    temp: document.querySelector("#result h1"),
+    windDirection: document.querySelector(".wind_direction"),
+    windSpeed: document.querySelector(".wind_speed"), 
+}
 
-// // CONSTANTS
+// CONSTANTS
 
-// const baseUrl = 'http://api.weatherapi.com/v1/current.json?key=5312652ec7f74e7d87440402252102'
+const baseUrl = 'http://api.weatherapi.com/v1/current.json?key=5312652ec7f74e7d87440402252102'
 
-// async function fetchWeather(location){
-//     const  stringResponse = await fetch(`${baseUrl}&q=${Midnapur}&aqi=no`)
-//     const response = await stringResponse.json()
+async function fetchWeather(location){
     
-// } 
+    const  stringResponse = await fetch(`${baseUrl}&q=${location}&aqi=no`)
+    const response = await stringResponse.json()
+    console.log(response)
+    const weather = response.current.condition.text
+    const humidity = response.current.humidity
+    const weatherIcon = response.current.condition.icon
+    const celsius = response.current.temp_c
+    const windDirection = response.current.wind_dir
+    const windSpeed = response.current.wind_kph
+    
+    elements.locationText.innerText = location.toUpperCase()
+    elements.weather.innerHTML = weather
+    elements.humidity.innerHTML = `HUMIDITY: ${humidity}`
+    elements.img.src = `https:${weatherIcon}`
+    elements.temp.innerHTML = `${celsius} &deg;`
+elements.windDirection.innerHTML = windDirection
+elements.windSpeed.innerHTML = windSpeed
+
+} 
 
 
-// // EVENT LISTENERS
+// EVENT LISTENERS
 
-// elements.searchBtn.addEventListener("click", ()=>{
-//     fetchWeather(elements.input.value)
-// })
-
+elements.searchBtn.addEventListener("click", ()=>{
+    fetchWeather(elements.input.value)
+})
+elements.input.addEventListener("keydown", (evt)=>{
+    if(evt.key === "Enter"){
+        fetchWeather(elements.input.value)
+    }
+})
